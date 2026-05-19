dd7f3c35e1d3eb27927ce2284ef076153e2df68d
Kyle <kyky.mortensen@gmail.com>
2026-05-19 16:01:08 -0600

    Add HTML preview page

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(express.static('public'));
 
 // Initialize Supabase Client using your environment variables
 const supabaseUrl = process.env.SUPABASE_URL;
